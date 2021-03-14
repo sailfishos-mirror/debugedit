71fe7ccfbb5188c5a17c86501c598909447ea9ad
Mark Wielaard <mark@klomp.org>
2021-03-14 16:56:04 +0100

    Add COPYING files for imported code
    Code for debugedit has been imported that was derived from binutils,
    libiberty and rpm some use different licenses, but all are upward
    compatible with GPLv3+.
    
    COPYING (GPLv2+) covers scripts/find-debuginfo.sh, all files under
    tests, tools/hashtab.h, tools/sha1.c, tools/md5.{c,h},
    tools/ansidecl.h and tools/debugedit.c.
    
    COPYING3 (GPLv3+) covers tools/sepdebugcrcfix.c and tools/sha1.h
    
    COPYING.LIB (LGPLv2+) covers tools/hashtab.c.

--- scripts/find-debuginfo.sh
+++ scripts/find-debuginfo.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
 #find-debuginfo.sh - automagically generate debug info and file list
 #for inclusion in an rpm spec file.
+
+# Copyright (C) 2002-2021 rpm and debugedit contributors
 #
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; version 2 of the License, or (at your
+# option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, see <http://www.gnu.org/licenses/>
+
 # Usage: find-debuginfo.sh [--strict-build-id] [-g] [-r] [-m] [-i] [-n]
 #			   [--keep-section SECTION] [--remove-section SECTION]
 #			   [--g-libs]
